ebe1deedf23a5c46fe006c5cb96c0629ed22bce6
xorrr <derxor@gmail.com>
2013-09-27 22:25:47 +0200

    add a database name String to UrlDB constructor

--- src/main/java/earth/xor/UrlDB.java
+++ src/main/java/earth/xor/UrlDB.java
@@ -1,15 +1,19 @@
 package earth.xor;
 
-import java.util.ArrayList;
-
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 
 public class UrlDB {
 
     private MongoClient mongo;
-    
-    public UrlDB(MongoClient mongo) {
+    private String databasename;
+
+    public UrlDB(MongoClient mongo, String dbname) {
 	this.mongo = mongo;
+	this.databasename = dbname;
     }
 
     public void addUrl(String string, String string2, String string3) {
