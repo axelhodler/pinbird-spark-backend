d64f10b8de885df5afad9e78e5e3897c4d1ecf35
xorrr <derxor@gmail.com>
2013-09-28 23:28:46 +0200

    add before and after in test where the restapi is started and stopped

---
+++ src/main/java/earth/xor/rest/SparkRestApi.java
@@ -0,0 +1,15 @@
+package earth.xor.rest;
+
+public class SparkRestApi {
+
+    public void launchServer() {
+	// TODO Auto-generated method stub
+	
+    }
+
+    public void stopServer() {
+	// TODO Auto-generated method stub
+	
+    }
+
+}
--- src/test/java/earth/xor/TestRestApi.java
+++ src/test/java/earth/xor/TestRestApi.java
@@ -1,15 +1,26 @@
 package earth.xor;
 
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
 
-import static com.jayway.restassured.RestAssured.*;
-import static com.jayway.restassured.matcher.RestAssuredMatchers.*;
-import static org.hamcrest.Matchers.*;
+import earth.xor.rest.SparkRestApi;
 
 public class TestRestApi {
 
+    private SparkRestApi restapi;
+    
+    @Before
+    public void setUpRestApi() {
+	restapi = new SparkRestApi();
+	restapi.launchServer();
+    }
+    
     @Test
     public void testAddingAUrlThroughTheRestApi() {
 	
@@ -17,5 +28,10 @@ public class TestRestApi {
 	given().parameters("url", "http://www.foo.org", "title", "foo",
 			"user", "user").expect()
 		.body("urls.title", equalTo("foo")).when().post("/urls");
+    }
+    
+    @After
+    public void stopRestApi() {
+	restapi.stopServer();
     }
 }
