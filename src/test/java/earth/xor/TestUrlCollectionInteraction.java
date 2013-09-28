9637ee6bb376c83ab307e5afd9ef1328092f2de4
xorrr <derxor@gmail.com>
2013-09-28 22:06:33 +0200

    add failing test when trying to add multiple urls

--- src/test/java/earth/xor/TestUrlCollectionInteraction.java
+++ src/test/java/earth/xor/TestUrlCollectionInteraction.java
@@ -6,12 +6,15 @@ import static org.junit.Assert.assertTrue;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
+import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
 
 public class TestUrlCollectionInteraction {
@@ -21,21 +24,21 @@ public class TestUrlCollectionInteraction {
 
     private MongoClient mongoClient;
     private UrlsCollection urlsCollection;
-    
+
     @BeforeClass
     public static void setUpEmbeddedMongo() throws UnknownHostException,
 	    IOException {
 	embeddedMongo = new EmbeddedMongo();
 	embeddedMongo.launchEmbeddedMongo(port);
     }
-    
+
     @Before
     public void setUpTests() throws UnknownHostException {
 	this.mongoClient = new MongoClient("localhost", port);
-	
+
 	this.urlsCollection = new UrlsCollection(mongoClient, "test");
     }
-    
+
     @Test
     public void testAccessingUrlDB() {
 	assertNotNull(urlsCollection.getMongoClient());
@@ -49,7 +52,27 @@ public class TestUrlCollectionInteraction {
 	urlsCollection.addUrl("http://www.foo.org", "foo", "user");
 	assertEquals(1, urlsCollection.getUrls().size());
     }
-    
+
+    @Test
+    public void testAddingAndGettingMultipleUrls() {
+	urlsCollection.addUrl("http://www.foo.org", "foo", "user");
+	urlsCollection.addUrl("http://www.bar.org", "bar", "user2");
+	urlsCollection.addUrl("http://www.baz.org", "baz", "user3");
+
+	DBCursor urlsCursor = urlsCollection.getUrls();
+
+	ArrayList<Url> urlList = new ArrayList<Url>();
+
+	while (urlsCursor.hasNext()) {
+	    Url currentUrl = new Url(urlsCursor.next().get("url").toString(), urlsCursor
+		    .next().get("title").toString(), urlsCursor.next().get("user").toString());
+	    urlList.add(currentUrl);
+	}
+	
+	assertEquals("bar", urlList.get(1).getTitle());
+	assertEquals("user3", urlList.get(2).getUser());
+    }
+
     @AfterClass
     public static void stopEmbeddedMongo() {
 	embeddedMongo.stopEmbeddedMongo();
