ca6a54b329d40700433883ecba4f86f8a59c96ed
xorrr <derxor@gmail.com>
2013-09-29 22:10:28 +0200

    add learning test parsing a jsonobject to string

--- src/test/java/earth/xor/learning/TestJsonSimple.java
+++ src/test/java/earth/xor/learning/TestJsonSimple.java
@@ -1,18 +1,29 @@
 package earth.xor.learning;
 
+import static org.junit.Assert.assertEquals;
+
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-import org.junit.Assert;
 import org.junit.Test;
 
 public class TestJsonSimple {
-    private String jsonExample = "{ \"url\":\"http:\\\\www.foo.org\", "
+    private String jsonExample = "{ \"url\":\"http://www.foo.org\", "
 	    + "\"title\":\"foo\", " + "\"user\":\"user\"}";
     
     @Test
     public void testGettingTheJsonObjectFromString() {
 	JSONObject url = (JSONObject) JSONValue.parse(jsonExample);
 	
-	Assert.assertEquals("foo", url.get("title"));
+	assertEquals("foo", url.get("title"));
+    }
+    
+    // A JSON-Object is an unordered set of name/value pairs
+    @Test
+    public void testParsingJsonObjectToString() {
+	JSONObject url = new JSONObject();
+	
+	url.put("title", "foo");
+	
+	assertEquals("{\"title\":\"foo\"}", url.toJSONString());
     }
 }
