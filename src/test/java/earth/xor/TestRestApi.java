774ee032e966bb0e34843fd514b0c29264b7d9eb
xorrr <derxor@gmail.com>
2013-10-01 01:23:49 +0200

    fail test trying to save the url and user via rest-api

--- src/test/java/earth/xor/TestRestApi.java
+++ src/test/java/earth/xor/TestRestApi.java
@@ -17,7 +17,7 @@ import com.jayway.restassured.RestAssured;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 import earth.xor.db.DbProperties;
@@ -26,47 +26,56 @@ import earth.xor.rest.SparkRestApi;
 public class TestRestApi {
 
     private SparkRestApi restapi;
-    
-    private String jsonTestString = "{\"title\":\"foo\"}"; 
-    
+
+    private String jsonTestString = "{\"url\":\"http://www.foo.org\","
+	    + "\"title\":\"foo\", \"user\":\"test\"}";
+
     private static int port = 12345;
     private static EmbeddedMongo embeddedMongo;
     private MongoClient mongoClient;
-    
+
     @BeforeClass
     public static void setUpEmbeddedMongo() throws UnknownHostException,
 	    IOException {
 	embeddedMongo = new EmbeddedMongo();
 	embeddedMongo.launchEmbeddedMongo(port);
     }
-    
+
     @Before
     public void setUpRestApi() throws UnknownHostException {
 	this.mongoClient = new MongoClient("localhost", port);
-	
+
 	restapi = new SparkRestApi(mongoClient);
 	restapi.launchServer();
     }
-    
+
     @Test
     public void testAddingAUrlThroughTheRestApi() {
-	
+
 	RestAssured.port = 4567;
-		
-	given().body(jsonTestString).expect().contentType("application/json")
-		.body("title", equalTo("foo")).when().post("/urls");
-	
+
+	given().body(jsonTestString)
+		.expect()
+		.contentType("application/json")
+		.body("url", equalTo("http://www.foo.org"), "title",
+			equalTo("foo"), "user", equalTo("test")).when()
+		.post("/urls");
+
 	DB urlsDb = mongoClient.getDB(DbProperties.DATABASE_NAME);
 	DBCollection col = urlsDb.getCollection("urls");
 
-	assertEquals("foo", col.findOne(new BasicDBObject("title", "foo")).get("title"));
+	DBObject foundEntry = col.findOne(new BasicDBObject("url", "http://www.foo.org"));
+	
+	assertEquals("http://www.foo.org", foundEntry.get("url"));
+	assertEquals("foo", foundEntry.get("title"));
+	assertEquals("test", foundEntry.get("user"));
     }
-    
+
     @After
     public void stopRestApi() {
 	restapi.stopServer();
     }
-    
+
     @AfterClass
     public static void stopEmbeddedMongo() {
 	embeddedMongo.stopEmbeddedMongo();
