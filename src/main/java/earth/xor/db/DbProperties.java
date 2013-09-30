a78fb96198d4193b37d07a48b8551aa1d0b8e086
xorrr <derxor@gmail.com>
2013-09-30 23:51:00 +0200

    refactor: avoid hardcoded strings via properties class

---
+++ src/main/java/earth/xor/db/DbProperties.java
@@ -0,0 +1,11 @@
+package earth.xor.db;
+
+public class DbProperties {
+    
+    public static final String DATABASE_NAME = "botarchive";
+    
+    public static final String URLSCOLLECTION_NAME = "urls";
+    public static final String URLSCOLLECTION_URL = "url";
+    public static final String URLSCOLLECTION_TITLE = "title";
+    public static final String URLSCOLLECTION_USER = "user";
+}
