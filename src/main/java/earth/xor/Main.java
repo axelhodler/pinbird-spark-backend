bd1b85a1b74d327b92694ae4c890e063e46b5d9c
xorrr <derxor@gmail.com>
2013-10-01 21:02:29 +0200

    add a main class which launches the server

---
+++ src/main/java/earth/xor/Main.java
@@ -0,0 +1,22 @@
+package earth.xor;
+
+import java.net.UnknownHostException;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+
+import earth.xor.rest.SparkRestApi;
+
+public class Main {
+
+    public static void main(String args[]) throws UnknownHostException {
+
+	ConfigAccessor config = new ConfigAccessor();
+	
+	MongoClientURI mongoUri = new MongoClientURI(config.getMongoUri());
+	MongoClient client = new MongoClient(mongoUri);
+	
+	SparkRestApi rest = new SparkRestApi(client);
+	rest.launchServer();
+    }
+}
