70bae536a96981b003461cf908504afdaa0e1bee
xorrr <derxor@gmail.com>
2013-10-01 03:03:13 +0200

    pass test by implementing the get method on the urls route

--- src/main/java/earth/xor/rest/SparkRestApi.java
+++ src/main/java/earth/xor/rest/SparkRestApi.java
@@ -1,7 +1,9 @@
 package earth.xor.rest;
 
+import static spark.Spark.get;
 import static spark.Spark.post;
 
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 
@@ -9,9 +11,8 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 import earth.xor.db.Url;
@@ -37,6 +38,31 @@ public class SparkRestApi {
 			.toString(), obj.get("user").toString()));
 
 		return request.body();
+	    }
+	});
+	
+	get(new Route("/urls") {
+
+	    @Override
+	    public Object handle(Request request, Response response) {
+		JSONArray array = new JSONArray();
+		
+		UrlsDatastore urls = new UrlsDatastore(mongoClient);
+		
+		DBCursor curs = urls.getUrls();
+		
+		while (curs.hasNext()) {
+		    JSONObject obj = new JSONObject();
+		    
+		    DBObject dbobj = curs.next();
+		    
+		    obj.put("url", dbobj.get("url"));
+		    obj.put("title", dbobj.get("title"));
+		    obj.put("user", dbobj.get("user"));
+		    
+		    array.add(obj);
+		}
+		return array.toJSONString();
 	    }
 	});
     }
