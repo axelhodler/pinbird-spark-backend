134d3598b594835b62a137e5732e9653e0f95d35
xorrr <derxor@gmail.com>
2013-10-01 19:31:10 +0200

    refactor testObjectToJsonConversion test

--- src/test/java/earth/xor/learning/TestGson.java
+++ src/test/java/earth/xor/learning/TestGson.java
@@ -2,6 +2,7 @@ package earth.xor.learning;
 
 import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.google.gson.Gson;
@@ -10,16 +11,25 @@ import earth.xor.db.Url;
 
 public class TestGson {
 
+    private String jsonExample = "{\"url\":\"testurl\","
+    	+ "\"title\":\"testtitle\","
+    	+ "\"user\":\"testuser\"}";
+    
+    private Url urlExample;
+
+    private Gson gson;
+    
+    @Before
+    public void setUpTests() {
+	this.gson = new Gson();
+	this.urlExample = new Url("testurl", "testtitle", "testuser");
+    }
+    
     @Test
     public void testObjectToJsonConversion() {
-	Url url = new Url("testurl", "testtitle", "testuser");
+		
+	String urlAsJson = this.gson.toJson(this.urlExample);
 	
-	Gson gson = new Gson();
-	
-	String urlAsJson = gson.toJson(url);
-	
-	assertEquals("{\"url\":\"testurl\","
-		+ "\"title\":\"testtitle\","
-		+ "\"user\":\"testuser\"}", urlAsJson);
+	assertEquals(jsonExample, urlAsJson);
     }
 }
