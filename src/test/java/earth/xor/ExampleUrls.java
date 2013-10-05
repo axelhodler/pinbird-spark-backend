f56f587311bdebbbd1a7a41ddc6d857fa6e9eddf
xorrr <derxor@gmail.com>
2013-10-05 17:48:11 +0200

    create a class with exampleurls for testing purposes

---
+++ src/test/java/earth/xor/ExampleUrls.java
@@ -0,0 +1,10 @@
+package earth.xor;
+
+import earth.xor.db.Url;
+
+public class ExampleUrls {
+    
+    public static final Url testUrl1 = new Url("http://www.foo.org", "foo", "user1");
+    public static final Url testUrl2 = new Url("http://www.bar.org", "bar", "user2");
+    public static final Url testUrl3 = new Url("http://www.baz.org", "baz", "user3");
+}
