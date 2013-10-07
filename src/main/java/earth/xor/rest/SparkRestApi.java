d5d6ed745c134ad25b58dcc615ab22a11197a8ab
xorrr <derxor@gmail.com>
2013-10-07 21:12:13 +0200

    pass test by adding the timestamp to the returned JSON Object

--- src/main/java/earth/xor/rest/SparkRestApi.java
+++ src/main/java/earth/xor/rest/SparkRestApi.java
@@ -15,6 +15,7 @@ import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
+import earth.xor.db.DbProperties;
 import earth.xor.db.Url;
 import earth.xor.db.UrlsDatastore;
 
@@ -36,82 +37,84 @@ public class SparkRestApi {
 
     public void createUrlsPostRoute() {
 	post(new Route("/urls") {
-	    
+
 	    @Override
 	    public Object handle(Request request, Response response) {
 
 		JSONObject obj = (JSONObject) JSONValue.parse(request.body());
 
-		urlsData.addUrl(new Url(obj.get("url").toString(), obj.get("title")
-			.toString(), obj.get("user").toString()));
+		urlsData.addUrl(new Url(obj.get("url").toString(), obj.get(
+			"title").toString(), obj.get("user").toString()));
 
 		addAccessControlAllowOriginHeader(response);
-		
+
 		return request.body();
 	    }
 	});
     }
-    
+
     public void createUrlsGetRoute() {
 	get(new Route("/urls") {
 
 	    @Override
 	    public Object handle(Request request, Response response) {
 		JSONArray array = new JSONArray();
-		
+
 		DBCursor curs = urlsData.getUrls();
-		
+
 		while (curs.hasNext()) {
 		    DBObject dbobj = curs.next();
-		    
+
 		    array.add(addDBObjectKeysToJsonObject(dbobj));
 		}
-		
+
 		JSONObject object = new JSONObject();
 		object.put("urls", array);
-		
+
 		addAccessControlAllowOriginHeader(response);
-		
+
 		return object.toJSONString();
 	    }
 
-	    
 	});
     }
-    
+
     private void createUrlsGetRouteForId() {
-	
+
 	get(new Route("/urls/:id") {
 
 	    @Override
 	    public Object handle(Request request, Response response) {
-		
+
 		DBObject foundUrl = urlsData.getUrlById(request.params(":id"));
 
 		JSONObject mainObject = new JSONObject();
-		
-		JSONObject innerObject = (JSONObject) JSONValue.parse(foundUrl.toString());
-		
+
+		JSONObject innerObject = (JSONObject) JSONValue.parse(foundUrl
+			.toString());
+
 		mainObject.put("url", innerObject);
-		
+
 		addAccessControlAllowOriginHeader(response);
-		
-		return mainObject.toJSONString();			
+
+		return mainObject.toJSONString();
 	    }
 	});
     }
-    
+
     private JSONObject addDBObjectKeysToJsonObject(DBObject dbObject) {
 	JSONObject jsonObject = new JSONObject();
-	
+
 	jsonObject.put("_id", dbObject.get("_id").toString());
 	jsonObject.put("url", dbObject.get("url"));
 	jsonObject.put("title", dbObject.get("title"));
 	jsonObject.put("user", dbObject.get("user"));
-	
-	return jsonObject;	
+	jsonObject.put(DbProperties.URLSCOLLECTION_TIMESTAMP,
+		dbObject.get(DbProperties.URLSCOLLECTION_TIMESTAMP).toString());
+
+	return jsonObject;
     }
-    
+
     private void addAccessControlAllowOriginHeader(Response response) {
 	response.header("Access-Control-Allow-Origin", "*");
     }
