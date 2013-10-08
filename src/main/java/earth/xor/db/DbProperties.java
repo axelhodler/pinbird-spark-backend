b92a601b9974e04a4383072d7569f48fdde2a83c
xorrr <derxor@gmail.com>
2013-10-08 17:32:26 +0200

    add _id to DbProperties

--- src/main/java/earth/xor/db/DbProperties.java
+++ src/main/java/earth/xor/db/DbProperties.java
@@ -7,6 +7,8 @@ public class DbProperties {
     public static String DATABASE_NAME = new ConfigAccessor().getDatabaseName();
     
     public static final String URLS_NAME = "urls";
+    
+    public static final String URLS_ID = "_id";
     public static final String URLS_URL = "url";
     public static final String URLS_TITLE = "title";
     public static final String URLS_USER = "user";
