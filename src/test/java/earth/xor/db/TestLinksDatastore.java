b3e5de24f7552b622c6889236c03d663987be9c4
xorrr <derxor@gmail.com>
2013-10-21 18:14:28 +0200

    organize imports in TestLinksDatastore

--- src/test/java/earth/xor/db/TestLinksDatastore.java
+++ src/test/java/earth/xor/db/TestLinksDatastore.java
@@ -13,7 +13,6 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.DBCursor;
 import com.mongodb.DBObject;
