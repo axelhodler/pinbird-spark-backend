c974469493c828350427ba54ae493c791065111d
xorrr <derxor@gmail.com>
2013-10-21 17:26:33 +0200

    reformat ConfigAccessor

--- src/main/java/earth/xor/ConfigAccessor.java
+++ src/main/java/earth/xor/ConfigAccessor.java
@@ -5,23 +5,23 @@ import java.io.IOException;
 import java.util.Properties;
 
 public class ConfigAccessor {
-    
+
     private Properties properties;
-    
+
     public ConfigAccessor() {
-	this.properties = new Properties();
-	try {
-	    this.properties.load(new FileInputStream("config.properties"));
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+        this.properties = new Properties();
+        try {
+            this.properties.load(new FileInputStream("config.properties"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
     public String getMongoUri() {
-	return this.properties.getProperty("uri");
+        return this.properties.getProperty("uri");
     }
-    
+
     public String getDatabaseName() {
-	return this.properties.getProperty("dbname");
+        return this.properties.getProperty("dbname");
     }
 }
