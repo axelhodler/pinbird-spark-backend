f3e11bff9c0c77e53d0e5bb8f58248ed2231d136
xorrr <derxor@gmail.com>
2013-10-21 18:19:57 +0200

    reformat LinksProp

--- src/main/java/earth/xor/db/LinksProp.java
+++ src/main/java/earth/xor/db/LinksProp.java
@@ -3,16 +3,16 @@ package earth.xor.db;
 import earth.xor.helpers.ConfigAccessor;
 
 public class LinksProp {
-    
+
     public static String DATABASE_NAME = new ConfigAccessor().getDatabaseName();
-    
+
     public static final String LINKS_NAME = "links";
-    
+
     public static final String ID = "_id";
     public static final String URL = "url";
     public static final String TITLE = "title";
     public static final String USER = "user";
     public static final String TIMESTAMP = "timestamp";
-    
+
     public static final String LINKS_ROUTE = "/links";
 }
