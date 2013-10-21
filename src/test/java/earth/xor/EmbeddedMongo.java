11c01d54ebb02ef5f09a1ffa1f10cbb4e8830c98
xorrr <derxor@gmail.com>
2013-10-21 16:58:52 +0200

    reformat EmbeddedMongo

--- src/test/java/earth/xor/EmbeddedMongo.java
+++ src/test/java/earth/xor/EmbeddedMongo.java
@@ -14,26 +14,27 @@ import de.flapdoodle.embed.process.runtime.Network;
 public class EmbeddedMongo {
 
     private MongodExecutable mongodExecutable;
-    
-    public void launchEmbeddedMongo(int port) throws UnknownHostException, IOException {
-	IMongodConfig mongodConfig = new MongodConfigBuilder()
-	.version(Version.V2_4_5)
-	.net(new Net(port, Network.localhostIsIPv6())).build();
-	
-	MongodStarter runtime = MongodStarter.getDefaultInstance();
-	
-	startMongoExecutable(runtime, mongodConfig);
+
+    public void launchEmbeddedMongo(int port) throws UnknownHostException,
+            IOException {
+        IMongodConfig mongodConfig = new MongodConfigBuilder()
+                .version(Version.V2_4_5)
+                .net(new Net(port, Network.localhostIsIPv6())).build();
+
+        MongodStarter runtime = MongodStarter.getDefaultInstance();
+
+        startMongoExecutable(runtime, mongodConfig);
     }
-    
+
     public void stopEmbeddedMongo() {
-	if (mongodExecutable != null) {
-	    mongodExecutable.stop();
-	}
+        if (mongodExecutable != null) {
+            mongodExecutable.stop();
+        }
     }
-    
+
     private void startMongoExecutable(MongodStarter runtime,
-	    IMongodConfig mongodConfig) throws IOException {
-	mongodExecutable = runtime.prepare(mongodConfig);
-	mongodExecutable.start();
+            IMongodConfig mongodConfig) throws IOException {
+        mongodExecutable = runtime.prepare(mongodConfig);
+        mongodExecutable.start();
     }
 }
