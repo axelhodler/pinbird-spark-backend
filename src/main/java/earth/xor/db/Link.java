6f3c366b17d585841a364fc7409202c768eea1a6
xorrr <derxor@gmail.com>
2014-03-07 20:15:49 +0100

    reformat Link class

--- src/main/java/earth/xor/db/Link.java
+++ src/main/java/earth/xor/db/Link.java
@@ -13,53 +13,53 @@ public class Link {
     private String objectId;
 
     public Link(String url, String title, String user, String string) {
-	setEveryThingButTimeAndId(url, title, user);
-	this.timestamp = string;
+        setEveryThingButTimeAndId(url, title, user);
+        this.timestamp = string;
     }
 
     public Link(String url, String title, String user) {
-	setEveryThingButTimeAndId(url, title, user);
+        setEveryThingButTimeAndId(url, title, user);
     }
 
     public String getUrl() {
-	return url;
+        return url;
     }
 
     public void setUrl(String url) {
-	this.url = url;
+        this.url = url;
     }
 
     public String getTitle() {
-	return title;
+        return title;
     }
 
     public void setTitle(String title) {
-	this.title = title;
+        this.title = title;
     }
 
     public String getUser() {
-	return user;
+        return user;
     }
 
     public void setUser(String user) {
-	this.user = user;
+        this.user = user;
     }
 
     public String getObjectId() {
-	return this.objectId;
+        return this.objectId;
     }
 
     public void setObjectId(String objectId) {
-	this.objectId = objectId;
+        this.objectId = objectId;
     }
 
     public String getTimeStamp() {
-	return this.timestamp;
+        return this.timestamp;
     }
 
     private void setEveryThingButTimeAndId(String url, String title, String user) {
-	this.url = url;
-	this.title = title;
-	this.user = user;
+        this.url = url;
+        this.title = title;
+        this.user = user;
     }
 }
