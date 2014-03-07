0a1268f01fc26d7548ea20a8f386978336448116
xorrr <derxor@gmail.com>
2014-03-07 19:42:25 +0100

    remove obsolete methods in TestMongoLinksDatastore

--- src/test/java/earth/xor/db/TestMongoLinksDatastore.java
+++ src/test/java/earth/xor/db/TestMongoLinksDatastore.java
@@ -1,7 +1,5 @@
 package earth.xor.db;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
@@ -10,12 +8,9 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.net.UnknownHostException;
-import java.util.ArrayList;
-import java.util.List;
 
 import org.bson.types.ObjectId;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
@@ -24,8 +19,6 @@ import org.mockito.runners.MockitoJUnitRunner;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
-import com.mongodb.DBObject;
 import com.mongodb.MongoClient;
 
 import earth.xor.ExampleLinks;
@@ -69,59 +62,5 @@ public class TestMongoLinksDatastore {
         linksData.getLinkById(new ObjectId().toString());
 
         verify(col, times(1)).findOne(any(BasicDBObject.class));
-    }
-
-    @Ignore
-    @Test
-    public void testAddingAndGettingAllLinks() {
-        linksData.addLink(ExampleLinks.testLink1);
-        linksData.addLink(ExampleLinks.testLink2);
-        linksData.addLink(ExampleLinks.testLink3);
-
-        DBCursor linksCursor = linksData.getLinks();
-
-        List<Link> linkList = createLinksArrayFromCursor(linksCursor);
-
-        assertEquals("bar", linkList.get(1).getTitle());
-        assertEquals("user3", linkList.get(2).getUser());
-
-        assertNotNull(linkList.get(1).getTimeStamp());
-    }
-
-    @Ignore
-    @Test
-    public void testGettingAUrlById() {
-        linksData.addLink(ExampleLinks.testLink1);
-
-        DBCollection col = mongoClient.getDB(LinkFields.DATABASE_NAME)
-                .getCollection(LinkFields.LINKS_NAME);
-
-        DBObject savedUrl = col.findOne();
-
-        DBObject obj = linksData.getLinkById(savedUrl.get(LinkFields.ID)
-                .toString());
-
-        assertEquals("foo", obj.get(LinkFields.TITLE).toString());
-        assertEquals(savedUrl.get(LinkFields.ID), obj.get(LinkFields.ID));
-    }
-
-    private List<Link> createLinksArrayFromCursor(DBCursor linksCurs) {
-        List<Link> linkList = new ArrayList<Link>();
-        iterateCursorToAddLinksToList(linksCurs, linkList);
-        return linkList;
-    }
-
-    private void iterateCursorToAddLinksToList(DBCursor linksCurs,
-            List<Link> linkList) {
-        while (linksCurs.hasNext()) {
-            DBObject dbo = linksCurs.next();
-            linkList.add(dbObjectToLink(dbo));
-        }
-    }
-
-    private Link dbObjectToLink(DBObject dbo) {
-        return new Link(dbo.get(LinkFields.URL).toString(), dbo.get(
-                LinkFields.TITLE).toString(), dbo.get(LinkFields.USER)
-                .toString(), dbo.get(LinkFields.TIMESTAMP).toString());
     }
 }
