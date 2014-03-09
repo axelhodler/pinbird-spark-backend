57dd4a5635551c1f5ed426f95db6643f3bda20f2
xorrr <derxor@gmail.com>
2014-03-09 22:02:24 +0100

    refactor Transformator class

--- src/main/java/earth/xor/rest/transformation/Transformator.java
+++ src/main/java/earth/xor/rest/transformation/Transformator.java
@@ -7,10 +7,25 @@ import earth.xor.db.Link;
 import earth.xor.db.LinkFields;
 
 public class Transformator {
-    public Link toLink(String jsonExample) {
-        JSONObject jo = (JSONObject) JSONValue.parse(jsonExample);
-        return new Link.Builder().url(jo.get(LinkFields.URL).toString())
-                .title(jo.get(LinkFields.TITLE).toString())
-                .user(jo.get(LinkFields.USER).toString()).build();
+    public Link toLink(String json) {
+        return createLink((JSONObject) JSONValue.parse(json));
+    }
+
+    private Link createLink(JSONObject json) {
+        return new Link.Builder().url(getUrl(json))
+                .title(getTitle(json))
+                .user(getUser(json)).build();
+    }
+
+    private String getUser(JSONObject json) {
+        return json.get(LinkFields.USER).toString();
+    }
+
+    private String getTitle(JSONObject json) {
+        return json.get(LinkFields.TITLE).toString();
+    }
+
+    private String getUrl(JSONObject json) {
+        return json.get(LinkFields.URL).toString();
     }
 }
