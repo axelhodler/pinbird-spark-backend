adad6d8c5dfaa778773119f39e91370107cb8d7d
xorrr <derxor@gmail.com>
2014-03-09 01:20:07 +0100

    fix main method according to new SparkRestApi implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,17 +5,21 @@ import org.xorrr.util.EnvironmentVars;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 
+import earth.xor.db.DatastoreFacade;
+import earth.xor.db.LinksDatastore;
+import earth.xor.db.MongoLinksDatastore;
 import earth.xor.rest.SparkRestApi;
 
 public class Main {
 
     public static void main(String args[]) throws UnknownHostException {
-
         MongoClientURI mongoUri = new MongoClientURI(
                 System.getenv(EnvironmentVars.URI));
         MongoClient client = new MongoClient(mongoUri);
+        LinksDatastore ds = new MongoLinksDatastore(client);
+        DatastoreFacade facade = new DatastoreFacade(ds);
 
-        SparkRestApi rest = new SparkRestApi();
-        rest.launchServer(client);
+        SparkRestApi rest = new SparkRestApi(facade);
+        rest.startApi();
     }
 }
