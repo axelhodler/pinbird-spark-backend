c5130328194d8af10064e63664c0b732117e94cc
xorrr <derxor@gmail.com>
2014-03-09 20:53:27 +0100

    begin implementing PostLinkRoute

---
+++ src/main/java/earth/xor/rest/PostLinkRoute.java
@@ -0,0 +1,23 @@
+package earth.xor.rest;
+
+import org.xorrr.util.EnvironmentVars;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PostLinkRoute extends Route{
+
+    public PostLinkRoute(String path) {
+        super(path);
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        String  pw = request.queryParams("pw");
+        if (!pw.equals(System.getenv(EnvironmentVars.PW)))
+            halt(401, "Authentication failed");
+        return null;
+    }
+
+}
---
+++ src/test/java/earth/xor/rest/TestPostLinkRoute.java
@@ -0,0 +1,55 @@
+package earth.xor.rest;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.xorrr.util.EnvironmentVars;
+
+import spark.AbstractRoute;
+import spark.Request;
+import spark.Response;
+import earth.xor.db.LinkFields;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ AbstractRoute.class })
+public class TestPostLinkRoute {
+    @Mock
+    Request req;
+    @Mock
+    Response resp;
+
+    private PostLinkRoute route;
+
+    @Before
+    public void setUp() {
+        PowerMockito.mockStatic(AbstractRoute.class);
+        route = new PostLinkRoute(LinkFields.LINKS_ROUTE);
+    }
+
+    @Test
+    public void linkCanBePosted() {
+        when(req.queryParams("pw")).thenReturn(System.getenv(EnvironmentVars.PW));
+
+        route.handle(req, resp);
+
+        verify(req, times(1)).queryParams("pw");
+    }
+
+    @Test
+    public void dontAuthWithWrongPassword() throws Exception {
+        when(req.queryParams("pw")).thenReturn("wrong");
+
+        route.handle(req, resp);
+
+        PowerMockito.verifyPrivate(AbstractRoute.class).invoke(401,
+                "Authentication failed");
+    }
+}
