fe1cd82850c2e29154ed7cf03a0a239c478bccbf
xorrr <derxor@gmail.com>
2014-03-09 21:56:55 +0100

    implement transformation from string to link in Transformator

--- src/main/java/earth/xor/rest/transformation/Transformator.java
+++ src/main/java/earth/xor/rest/transformation/Transformator.java
@@ -1,5 +1,16 @@
 package earth.xor.rest.transformation;
 
-public class Transformator {
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 
+import earth.xor.db.Link;
+import earth.xor.db.LinkFields;
+
+public class Transformator {
+    public Link toLink(String jsonExample) {
+        JSONObject jo = (JSONObject) JSONValue.parse(jsonExample);
+        return new Link.Builder().url(jo.get(LinkFields.URL).toString())
+                .title(jo.get(LinkFields.TITLE).toString())
+                .user(jo.get(LinkFields.USER).toString()).build();
+    }
 }
--- src/test/java/earth/xor/rest/transformation/TestTransformator.java
+++ src/test/java/earth/xor/rest/transformation/TestTransformator.java
@@ -1,12 +1,30 @@
 package earth.xor.rest.transformation;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import earth.xor.db.Link;
 
 public class TestTransformator {
 
+    private Transformator trans;
+    private String jsonExample = "{ \"url\":\"http://www.foo.org\", "
+            + "\"title\":\"foo\", " + "\"user\":\"user\"}";
+
+    @Before
+    public void setUp() {
+        trans = new Transformator();
+    }
+
     @Test
-    public void test() {
-        Transformator trans = new Transformator();
+    public void canTransformStringToLink() {
+        Link l = trans.toLink(jsonExample);
+
+        assertEquals("http://www.foo.org", l.getUrl());
+        assertEquals("foo", l.getTitle());
+        assertEquals("user", l.getUser());
     }
 
 }
