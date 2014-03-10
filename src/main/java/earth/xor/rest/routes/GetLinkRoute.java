f99027fdaae370b46a4438029da1dd5b47f7b4cd
xorrr <derxor@gmail.com>
2014-03-10 22:45:28 +0100

    create basic GetLinkRoute

---
+++ src/main/java/earth/xor/rest/routes/GetLinkRoute.java
@@ -0,0 +1,20 @@
+package earth.xor.rest.routes;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class GetLinkRoute extends Route {
+
+    public GetLinkRoute(String path) {
+        super(path);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
---
+++ src/test/java/earth/xor/rest/routes/TestGetLinkRoute.java
@@ -0,0 +1,12 @@
+package earth.xor.rest.routes;
+
+import org.junit.Test;
+
+public class TestGetLinkRoute {
+
+    @Test
+    public void test() {
+        GetLinkRoute route = new GetLinkRoute("asdf");
+    }
+
+}
