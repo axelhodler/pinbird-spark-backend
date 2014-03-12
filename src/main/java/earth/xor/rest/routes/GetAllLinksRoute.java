1ba7a15432c0270d09318a25024e395496b8b47f
xorrr <derxor@gmail.com>
2014-03-12 01:28:33 +0100

    implement GetAllLinksRoute handle method

--- src/main/java/earth/xor/rest/routes/GetAllLinksRoute.java
+++ src/main/java/earth/xor/rest/routes/GetAllLinksRoute.java
@@ -1,6 +1,13 @@
 package earth.xor.rest.routes;
 
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import earth.xor.db.DatastoreFacade;
+import earth.xor.db.Link;
+import earth.xor.db.LinkFields;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -14,10 +21,26 @@ public class GetAllLinksRoute extends Route {
         this.facade = facade;
     }
 
+    @SuppressWarnings("unchecked")
     @Override
     public Object handle(Request request, Response response) {
-        facade.getLinks();
-        return null;
+        List<Link> links = facade.getLinks();
+
+        JSONArray linksArray = new JSONArray();
+        for (Link l : links) {
+            JSONObject jo = new JSONObject();
+            jo.put(LinkFields.ID, l.getObjectId());
+            jo.put(LinkFields.URL, l.getUrl());
+            jo.put(LinkFields.TITLE, l.getTitle());
+            jo.put(LinkFields.USER, l.getUser());
+            jo.put(LinkFields.TIMESTAMP, l.getTimeStamp());
+            linksArray.add(jo);
+        }
+
+        JSONObject mainJsonObject = new JSONObject();
+        mainJsonObject.put("links", linksArray);
+
+        return mainJsonObject.toJSONString();
     }
 
 }
--- src/test/java/earth/xor/rest/routes/TestGetAllLinksRoute.java
+++ src/test/java/earth/xor/rest/routes/TestGetAllLinksRoute.java
@@ -1,17 +1,28 @@
 package earth.xor.rest.routes;
 
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.xorrr.util.LinkObjects;
 
 import spark.Request;
 import spark.Response;
 import earth.xor.db.DatastoreFacade;
+import earth.xor.db.Link;
+import earth.xor.db.LinkFields;
 
 @RunWith(PowerMockRunner.class)
 public class TestGetAllLinksRoute {
@@ -31,8 +42,28 @@ public class TestGetAllLinksRoute {
 
     @Test
     public void canGetLink() {
-        route.handle(req, resp);
+        List<Link> links = new ArrayList<>();
+        links.add(LinkObjects.testLink1);
+        links.add(LinkObjects.testLink2);
+        JSONArray linksArray = new JSONArray();
+        for (Link l : links) {
+            JSONObject jo = new JSONObject();
+            jo.put(LinkFields.ID, l.getObjectId());
+            jo.put(LinkFields.URL, l.getUrl());
+            jo.put(LinkFields.TITLE, l.getTitle());
+            jo.put(LinkFields.USER, l.getUser());
+            jo.put(LinkFields.TIMESTAMP, l.getTimeStamp());
+            linksArray.add(jo);
+        }
+
+        JSONObject mainJsonObject = new JSONObject();
+        mainJsonObject.put("links", linksArray);
+
+        when(facade.getLinks()).thenReturn(links);
+
+        Object json = route.handle(req, resp);
 
         verify(facade, times(1)).getLinks();
+        assertEquals(json, mainJsonObject.toJSONString());
     }
 }
