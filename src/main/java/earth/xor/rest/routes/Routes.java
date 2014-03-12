2a2775a026c0ac502b2b19d9dbf720bf9060e1be
xorrr <derxor@gmail.com>
2014-03-12 01:44:47 +0100

    add new constant route string to Routes

--- src/main/java/earth/xor/rest/routes/Routes.java
+++ src/main/java/earth/xor/rest/routes/Routes.java
@@ -3,4 +3,5 @@ package earth.xor.rest.routes;
 public abstract class Routes {
     public final static String POST_LINK = "/links";
     public final static String GET_ALL_LINKS = "/links";
+    public final static String GET_LINK_BY_ID = "/links/:id";
 }
