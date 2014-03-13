510ed42a66b827c6c74d9224316c46ecdab31c63
xorrr <derxor@gmail.com>
2014-03-14 00:41:31 +0100

    add IngerationTest interface to later skip integration tests in a normal mvn testrun

---
+++ src/test/java/earth/xor/helpers/IntegrationTest.java
@@ -0,0 +1,13 @@
+package earth.xor.helpers;
+
+/**
+ * Used to exclude the integration-tests when just invoking mvn test
+ * 
+ * for running all tests (integration tests included) use: mvn integration-test
+ * 
+ * @author xorrr
+ * 
+ */
+public interface IntegrationTest {
+
+}
