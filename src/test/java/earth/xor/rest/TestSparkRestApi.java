6d8f4382326e9fd125ea83042c1b4a6703e0f3fc
xorrr <derxor@gmail.com>
2014-03-14 23:37:17 +0100

    organize imports in TestSparkRestApi

--- src/test/java/earth/xor/rest/TestSparkRestApi.java
+++ src/test/java/earth/xor/rest/TestSparkRestApi.java
@@ -1,28 +1,18 @@
 package earth.xor.rest;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static spark.Spark.setPort;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.powermock.api.mockito.PowerMockito;
-import org.powermock.core.classloader.annotations.PrepareForTest;
-import org.powermock.modules.junit4.PowerMockRunner;
 import org.xorrr.util.EnvironmentVars;
 
-import spark.Route;
-import spark.Spark;
-import earth.xor.db.DatastoreFacade;
 import earth.xor.rest.routes.GetAllLinksRoute;
 import earth.xor.rest.routes.GetLinkByIdRoute;
 import earth.xor.rest.routes.PostLinkRoute;
-import earth.xor.rest.transformation.Transformator;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestSparkRestApi {
