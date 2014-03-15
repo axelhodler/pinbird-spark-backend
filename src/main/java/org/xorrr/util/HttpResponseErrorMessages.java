b424ad20aa398b8920c91fcbb5ff098443995403
xorrr <derxor@gmail.com>
2014-03-15 18:56:17 +0100

    add constant for missing payload error msg

--- src/main/java/org/xorrr/util/HttpResponseErrorMessages.java
+++ src/main/java/org/xorrr/util/HttpResponseErrorMessages.java
@@ -3,4 +3,5 @@ package org.xorrr.util;
 public abstract class HttpResponseErrorMessages {
     public static final String MISSING_PW = "Provide a password parameter";
     public static final String AUTH_FAIL = "Authentication failed";
+    public static final String MISSING_PAYLOAD = "Payload missing";
 }
