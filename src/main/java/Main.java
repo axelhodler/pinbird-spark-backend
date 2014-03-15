31192a699b407a7574c7a5e1c6c4a66412136985
xorrr <derxor@gmail.com>
2014-03-15 01:02:52 +0100

    set up main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,9 @@ import earth.xor.db.LinksDatastore;
 import earth.xor.db.MongoLinksDatastore;
 import earth.xor.rest.RestApi;
 import earth.xor.rest.SparkFacade;
+import earth.xor.rest.routes.GetAllLinksRoute;
+import earth.xor.rest.routes.GetLinkByIdRoute;
+import earth.xor.rest.routes.PostLinkRoute;
 import earth.xor.rest.transformation.Transformator;
 
 public class Main {
@@ -18,9 +21,15 @@ public class Main {
         MongoClientURI mongoUri = new MongoClientURI(
                 System.getenv(EnvironmentVars.URI));
         MongoClient client = new MongoClient(mongoUri);
+
         LinksDatastore ds = new MongoLinksDatastore(client);
         DatastoreFacade facade = new DatastoreFacade(ds);
+        Transformator transformator = new Transformator();
 
         RestApi rest = new RestApi(new SparkFacade());
+        rest.setPort(Integer.valueOf(System.getenv(EnvironmentVars.PORT)));
+        rest.createPOSTlinksRoute(new PostLinkRoute(facade, transformator));
+        rest.createGETlinkByIdRoute(new GetLinkByIdRoute(facade, transformator));
+        rest.createGETlinksRoute(new GetAllLinksRoute(facade, transformator));
     }
 }
