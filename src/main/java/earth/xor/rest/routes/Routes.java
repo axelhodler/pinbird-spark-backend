8fb53aca31d81fa035e90839909bc24e05af06c7
xorrr <derxor@gmail.com>
2014-03-16 23:05:13 +0100

    add options links route constant to Routes class

--- src/main/java/earth/xor/rest/routes/Routes.java
+++ src/main/java/earth/xor/rest/routes/Routes.java
@@ -4,4 +4,5 @@ public abstract class Routes {
     public final static String POST_LINK = "/links";
     public final static String GET_ALL_LINKS = "/links";
     public final static String GET_LINK_BY_ID = "/links/:id";
+    public static final String OPTIONS_LINKS = "/links";
 }
