69d30d7d3fe8df6d602f4594c154a9309ff021ff
xorrr <derxor@gmail.com>
2014-03-16 23:16:57 +0100

    implement OptionsRoute

---
+++ src/main/java/earth/xor/rest/routes/OptionsRoute.java
@@ -0,0 +1,21 @@
+package earth.xor.rest.routes;
+
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class OptionsRoute extends Route {
+
+    public OptionsRoute() {
+        super(Routes.OPTIONS_LINKS);
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        response.header("Access-Control-Allow-Methods", "GET, POST");
+        response.header("Access-Control-Allow-Origin", "*");
+
+        return request.body();
+    }
+
+}
---
+++ src/test/java/earth/xor/rest/routes/TestOptionsRoute.java
@@ -0,0 +1,48 @@
+package earth.xor.rest.routes;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import spark.Request;
+import spark.Response;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TestOptionsRoute {
+    @Mock
+    Request req;
+    @Mock
+    Response resp;
+
+    private OptionsRoute route;
+
+    @Before
+    public void setUp() {
+        route = new OptionsRoute();
+    }
+
+    @Test
+    public void accessControlAllowMethodsUsed() {
+        when(req.body()).thenReturn("something");
+
+        route.handle(req, resp);
+
+        verify(resp, times(1)).header("Access-Control-Allow-Methods",
+                "GET, POST");
+    }
+
+    @Test
+    public void accessControlAllowOriginHeaderUsed() {
+        when(req.body()).thenReturn("something");
+
+        route.handle(req, resp);
+
+        verify(resp, times(1)).header("Access-Control-Allow-Origin", "*");
+    }
+}
