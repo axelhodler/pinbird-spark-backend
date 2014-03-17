d366cc4c203407ed8a2a2322b5abfc220e6f9392
xorrr <derxor@gmail.com>
2014-03-17 20:37:26 +0100

    create JsonAccessor for testing to save the different json strings in extra files

---
+++ src/test/java/org/xorrr/util/JsonAccessor.java
@@ -0,0 +1,26 @@
+package org.xorrr.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import org.apache.commons.io.IOUtils;
+import org.xorrr.integrationtests.TestRestApi;
+
+public class JsonAccessor {
+
+    public static String getPostRequestBody() {
+        String jsonString = null;
+
+        try {
+            File testFile = new File(TestRestApi.class.getResource(
+                    "/linksPost.JSON").toURI());
+            jsonString = IOUtils.toString(new FileInputStream(testFile));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        return jsonString;
+    }
+}
