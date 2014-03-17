6e89bdca8d83a1d0d3b8d4646da3cc1de4f5a99f
xorrr <derxor@gmail.com>
2014-03-17 18:31:02 +0100

    add Authorization header to header keys

--- src/main/java/org/xorrr/util/HttpHeaderKeys.java
+++ src/main/java/org/xorrr/util/HttpHeaderKeys.java
@@ -4,4 +4,5 @@ public abstract class HttpHeaderKeys {
     public static final String ACAMethods = "Access-Control-Allow-Methods";
     public static final String ACAOrigin = "Access-Control-Allow-Origin";
     public static final String ACAHeaders = "Access-Control-Allow-Headers";
+    public static final String Authorization = "Authorization";
 }
