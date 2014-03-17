edae97fab235f546199a8db0c5a26c0db5f2fe4e
xorrr <derxor@gmail.com>
2014-03-17 20:46:21 +0100

    refactor JsonAccessor

--- src/test/java/org/xorrr/util/JsonAccessor.java
+++ src/test/java/org/xorrr/util/JsonAccessor.java
@@ -11,30 +11,23 @@ import org.xorrr.integrationtests.TestRestApi;
 public class JsonAccessor {
 
     public static String getPostRequestBody() {
-        String jsonString = null;
-
-        try {
-            File testFile = new File(TestRestApi.class.getResource(
-                    "/linksPost.JSON").toURI());
-            jsonString = IOUtils.toString(new FileInputStream(testFile));
-        } catch (IOException | URISyntaxException e) {
-            e.printStackTrace();
-        }
-
-        return jsonString;
+        return fileToString("linksPost.JSON");
     }
 
     public static String getExampleLink() {
+        return fileToString("link.json");
+    }
+
+    private static String fileToString(String filename) {
         String jsonString = null;
 
         try {
             File testFile = new File(TestRestApi.class.getResource(
-                    "/link.json").toURI());
+                    "/" + filename).toURI());
             jsonString = IOUtils.toString(new FileInputStream(testFile));
         } catch (IOException | URISyntaxException e) {
             e.printStackTrace();
         }
-
         return jsonString;
     }
 }
