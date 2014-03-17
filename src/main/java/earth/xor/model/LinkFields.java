7f1761a4dc79135a7e37605188c6b78828faf285
xorrr <derxor@gmail.com>
2014-03-17 20:59:53 +0100

    add LINK constant to LinkFields

--- src/main/java/earth/xor/model/LinkFields.java
+++ src/main/java/earth/xor/model/LinkFields.java
@@ -7,6 +7,7 @@ public abstract class LinkFields {
     public static String DATABASE_NAME = System.getenv(EnvironmentVars.DB_NAME);
 
     public static final String LINKS_NAME = "links";
+    public static final String LINK = "link";
 
     public static final String ID = "_id";
     public static final String URL = "url";
