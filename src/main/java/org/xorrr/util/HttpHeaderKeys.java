65ad71e464783141997915883feeb50f1526684b
xorrr <derxor@gmail.com>
2014-03-17 18:13:45 +0100

    add class with constants for the keys of http headers

---
+++ src/main/java/org/xorrr/util/HttpHeaderKeys.java
@@ -0,0 +1,7 @@
+package org.xorrr.util;
+
+public abstract class HttpHeaderKeys {
+    public static final String ACAMethods = "Access-Control-Allow-Methods";
+    public static final String ACAOrigin = "Access-Control-Allow-Origin";
+    public static final String ACAHeaders = "Access-Control-Allow-Headers";
+}
