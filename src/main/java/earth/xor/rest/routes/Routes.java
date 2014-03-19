8f1cab36a1c1d2f387c717af3c5f15cdf8427ec1
xorrr <derxor@gmail.com>
2014-03-19 21:13:07 +0100

    fix DELETE_BOOKMARK route constant

--- src/main/java/earth/xor/rest/routes/Routes.java
+++ src/main/java/earth/xor/rest/routes/Routes.java
@@ -7,5 +7,5 @@ public abstract class Routes {
     public static final String GET_ALL_BOOKMARKS = BASE;
     public static final String GET_BOOKMARKS_BY_ID = BASE + "/:id";
     public static final String OPTIONS_BOOKMARKS = BASE;
-    public static final String DELETE_BOOKMARK = BASE;
+    public static final String DELETE_BOOKMARK = BASE + "/:id";
 }
